c3f2887c155800288202e285a4cb234379351bba
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2026-01-12 00:34:20 +0530

    PROXY FIX

--- app.py
+++ app.py
@@ -5,9 +5,22 @@ from langchain_classic.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
 from langchain_groq import ChatGroq
 import os
+from pytube import YouTube
 from dotenv import load_dotenv
 load_dotenv()
 YOUTUBE_PROXY = os.getenv("YOUTUBE_PROXY")
+
+proxy={"http": YOUTUBE_PROXY, "https": YOUTUBE_PROXY} if YOUTUBE_PROXY else None
+
+original_init = YouTube.__init__
+
+
+def patched_init(self, url, *args, **kwargs):
+    kwargs["proxies"] = proxy
+    original_init(self, url, *args, **kwargs)
+
+YouTube.__init__ = patched_init
+
 
 # Streamlit app
 st.set_page_config(page_title="LangChain: Summarize Text From YouTube or Websites",page_icon="🐧")
@@ -42,7 +55,7 @@ if st.button("Summarize the Content from YT or Website"):
             with st.spinner("Waiting..."):
                 # Loading the website or yt video data
                 if "youtube.com" in url:
-                    loader=YoutubeLoader.from_youtube_url(youtube_url=url,add_video_info=False,proxies={"http": YOUTUBE_PROXY, "https": YOUTUBE_PROXY} if YOUTUBE_PROXY else None) #add_video_info=True-->Tells us weather we want to add video info or not
+                    loader=YoutubeLoader.from_youtube_url(youtube_url=url,add_video_info=False) #add_video_info=True-->Tells us weather we want to add video info or not
                 else:
                     loader=UnstructuredURLLoader(urls=[url],ssl_verified=False,
                                                  headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
