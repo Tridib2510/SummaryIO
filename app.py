2f8103b267d4a96b0b998500ac26fc8ab8957b79
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2026-01-12 13:38:25 +0530

    deployment

--- app.py
+++ app.py
@@ -4,28 +4,14 @@ from langchain_classic.prompts import PromptTemplate
 from langchain_classic.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
 from langchain_groq import ChatGroq
-import os
-from pytube import YouTube
 from dotenv import load_dotenv
+
 load_dotenv()
-YOUTUBE_PROXY = os.getenv("YOUTUBE_PROXY")
-
-proxy={"http": YOUTUBE_PROXY, "https": YOUTUBE_PROXY} if YOUTUBE_PROXY else None
-
-original_init = YouTube.__init__
-
-
-def patched_init(self, url, *args, **kwargs):
-    kwargs["proxies"] = proxy
-    original_init(self, url, *args, **kwargs)
-
-YouTube.__init__ = patched_init
-
-
 # Streamlit app
 st.set_page_config(page_title="LangChain: Summarize Text From YouTube or Websites",page_icon="🐧")
 st.title("LangChain: Summarize Text From YT or Website")
 st.subheader("Summarize URL")
+
 
 #Get the Groq API Key and url(YT or website)to be summarized
 with st.sidebar:
@@ -54,14 +40,17 @@ if st.button("Summarize the Content from YT or Website"):
         try:
             with st.spinner("Waiting..."):
                 # Loading the website or yt video data
+                docs=None
                 if "youtube.com" in url:
-                    loader=YoutubeLoader.from_youtube_url(youtube_url=url,add_video_info=False) #add_video_info=True-->Tells us weather we want to add video info or not
+                    loader=YoutubeLoader.from_youtube_url(youtube_url=url,add_video_info=False)
+                    docs = loader.load()
+                    #add_video_info=True-->Tells us weather we want to add video info or not
                 else:
                     loader=UnstructuredURLLoader(urls=[url],ssl_verified=False,
                                                  headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
                     # When we are hitting the particular url the web server is also demanding what kind of headers we put on top of it like Which user agent
                     # Those headers are there to make your request look like it’s coming from a real web browser, not a bot. This is extremely important when scraping URLs.
-                docs=loader.load()
+                    docs=loader.load()
 
                 ##Chain for Summarization
                 chain=load_summarize_chain(llm,chain_type='stuff',prompt=prompt)
@@ -73,4 +62,3 @@ if st.button("Summarize the Content from YT or Website"):
                     
 
                     
-
